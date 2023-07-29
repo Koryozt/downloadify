dabdefed1f838d1dec850d9bc218a5eeab8a7abe
Gustavo Silva <gustavoasc2004@gmail.com>
2023-07-29 02:17:48 -0400

    Finished the script

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import requests
 import json
 import base64
+import argparse
+import yt_dlp
+import urllib
+import subprocess
+import re
+import socket
+import os
 
-# 0uf6Hev3fqAa1V2AZU5Y5i
+# 6fAcSQBDK5W8Wm7v2Zqu4t
 
 AUTH_URL = 'https://accounts.spotify.com/api/token'
 API_BASE = "https://api.spotify.com/v1/"
+YOUTUBE_SEARCH_QUERY_URL = 'https://www.youtube.com/results?search_query='
 
 def main():
 
-    playlist_id = input("id:")
+    ap = argparse.ArgumentParser()
+    ap.add_argument('-pid', '--playlist_id', required = True, help = 'Spotify playlist id to download')
+    ap.add_argument('-dir', '--directory', required=True, help = "The directory to save the downloaded files")
+
+    args = vars(ap.parse_args())
+
+    playlist_id = args['playlist_id']
+    directory = args['directory']
 
     with open('client.txt') as f:
         lines = f.readlines()
@@ -62,13 +77,51 @@ def main():
 
     name = playlist['name']
     count = playlist['tracks']['total']
+    tracks_group = playlist['tracks']['items']
+
+    tracks = list()
+
+    for track_item in tracks_group:
+        track_name = track_item['track']['name']
+        track_artist = track_item['track']['artists'][0]['name']
+
+        tracks.append(f'{track_name} by {track_artist} lyrics')
 
     print(f"Playlist: {name} \n"+ 
         f"Track count: {count}")
 
-    
+    for track in tracks:
+        search_q = urllib.parse.urlencode({'search_query':track})
+        body = (requests.get(YOUTUBE_SEARCH_QUERY_URL + search_q))
+        video_href = re.findall(r"watch\?v=(\S{11})", body.text)
+        
+        try:
+            video_url = 'https://www.youtube.com/watch?v=' + video_href[0]
 
+            if directory == '.' or not directory:
+                filename = f'./downloads/{name}/{track}.mp3'
+            else:
+                filename = directory + f'./{name}/{track}.mp3'
 
+            if os.path.exists(filename):
+                print(f'{filename} already exists')
+                continue
+            
+            video_info = yt_dlp.YoutubeDL().extract_info(url = video_url,download=False)
+
+            options={
+                'format':'bestaudio/best',
+                'keepvideo':False,
+                'outtmpl':filename,
+            }
+
+            with yt_dlp.YoutubeDL(options) as ydl:
+                ydl.download([video_info['webpage_url']])
+
+            print(f'Download complete... {filename}')
+
+        except IndexError as e:
+            print(e)
 
 if __name__ == '__main__':
 	main()
