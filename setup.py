37e4f9dcd9a65330b9bbe62f30313178dca6286b
Gustavo Silva <gustavoasc2004@gmail.com>
2023-07-29 13:36:03 -0400

    Modified the requirements

--- setup.py
+++ setup.py
@@ -3,16 +3,18 @@ from setuptools import setup, find_packages
 setup(
 	name='downloadify',
 	version='1.0.0',
-	description='Download your spotify playlists and songs easily, store them in any folder you want, and even in your phone',
+	description='Download your spotify playlists with a single python script in any folder do you want',
+    url='https://github.com/Koryozt/downloadify',
     author='kzt',
     author_email='gustavoasc2004@gmail.com',
     packages=find_packages(),
     install_requires=[
         'requests',
+        'yt-dlp'
     ],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: End-Users',
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.11',
